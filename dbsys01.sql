ddf3f36429048bebdf20d3a6ec028c967f03a81a
unknown <ja852ber@FHKN.DE>
2019-11-29 14:34:50 +0100

    change animeshs shit code

--- dbsys01.sql
+++ dbsys01.sql
@@ -5,20 +5,20 @@
 
 CREATE TABLE Touristenattraktion
 ( 
-attraktionsName varchar(30),
+attraktionsName varchar2(30),
 CONSTRAINT pk_Touristenattraktion PRIMARY KEY (attraktionsName)
 );
 
 CREATE TABLE Land
 ( 
-name varchar(20),
+name varchar2(20),
 CONSTRAINT pk_Land PRIMARY KEY (name)
 );
 
 CREATE TABLE Adresse
 ( 
 addressId INTEGER,
-postleitzahl INTEGER,
+postleitzahl varchar2(30), --Int zu varchar2--
 straße varchar2(30),
 hausnummer varchar2(3),
 landName varchar2(20),
@@ -44,8 +44,8 @@ CONSTRAINT fk_Ferienwohnung FOREIGN KEY (addressId)
 CREATE TABLE EntferntVon
 ( 
 entfernung INTEGER,
-attraktionsName  varchar(30),
-ferienwohnungsName varchar(20),
+attraktionsName  varchar2(30),
+ferienwohnungsName varchar2(20),
 CONSTRAINT fk_attraktionsName FOREIGN KEY (attraktionsName)
            REFERENCES Touristenattraktion(attraktionsName)
            ON DELETE CASCADE,
@@ -58,10 +58,10 @@ CONSTRAINT entfernungCheck CHECK (entfernung > 0)
 
 CREATE TABLE Kunde
 ( 
-mailadresse  varchar(30),
-Passwort  varchar(30),
-IBAN  varchar(30),   --eindeutig--
-Kundenname  varchar(30),
+mailadresse  varchar2(30),
+Passwort  varchar2(30),
+IBAN  varchar2(30),   --eindeutig--
+Kundenname  varchar2(30),
 addressId INTEGER,
 
 CONSTRAINT pk_mailadresse PRIMARY KEY (mailadresse),
@@ -84,25 +84,31 @@ CONSTRAINT sterneCheck CHECK (sterne>=0 OR sterne<6)
 --needs check--
 CREATE TABLE Buchung
 ( 
-buchungsNummer varchar(30),
-datum DATE,
-abreiseDatum DATE,
-anreiseDatum DATE,
-ferienwohnungsName varchar(30),
-mailadresse varchar(30),
-betrag INTEGER,
-rechnungsnummer INTEGER,
-bewertungsId INTEGER,
+buchungsNummer varchar2(30) NOT NULL,
+datum DATE NOT NULL,
+abreiseDatum DATE NOT NULL,
+anreiseDatum DATE NOT NULL,
+ferienwohnungsName varchar2(30) NOT NULL,
+mailadresse varchar2(30) NOT NULL,
+betrag INTEGER NOT NULL,
+rechnungsnummer INTEGER NOT NULL,
+bewertungsId INTEGER NOT NULL,
+rechnungsdatum DATE NOT NULL,
 
 CONSTRAINT pk_Buchung PRIMARY KEY (buchungsNummer),
 
 CONSTRAINT fk_Buchungs_ferienwohnungsName FOREIGN KEY (ferienwohnungsName)
            REFERENCES Ferienwohnung(name),
+
 CONSTRAINT fk_mailadresse FOREIGN KEY (mailadresse)
            REFERENCES Kunde(mailadresse),
+
 CONSTRAINT fk_Buchung_bewertungsId FOREIGN KEY (bewertungsId)
            REFERENCES Bewertung(bewertungsId),           
-CONSTRAINT BetragCheck CHECK (betrag > 0)           
+
+CONSTRAINT BetragCheck CHECK (betrag > 0),
+
+CONSTRAINT RechnungsNrCheck CHECK (rechnungsnummer > 0)
 );
 
 CREATE TABLE Anzahlung
@@ -110,7 +116,7 @@ CREATE TABLE Anzahlung
 anzahlungsId INTEGER,
 betrag DOUBLE PRECISION,
 datum DATE,
-buchungsNummer varchar(30),
+buchungsNummer INTEGER,
 
 CONSTRAINT pk_anzahlungsId PRIMARY KEY (anzahlungsId),
 CONSTRAINT anzahlungsIdCheck CHECK (anzahlungsId > 0),
@@ -121,7 +127,7 @@ CONSTRAINT fk_anzahlung FOREIGN KEY (buchungsNummer)
 
 CREATE TABLE Ausstattung
 ( 
-austattungsName varchar(30),
+austattungsName varchar2(30),
 
 CONSTRAINT pk_ausstatttung PRIMARY KEY (austattungsName)
 );
@@ -129,18 +135,26 @@ CONSTRAINT pk_ausstatttung PRIMARY KEY (austattungsName)
 
 CREATE TABLE Besitzt
 ( 
-austattungsName varchar(30),
-ferienwohnungsName varchar(30),
+austattungsName varchar2(30),
+ferienwohnungsName varchar2(30),
 
 CONSTRAINT fk_besitzt_austattungsName FOREIGN KEY (austattungsName)
-           REFERENCES Ausstattung(austattungsName),
+           REFERENCES Ausstattung(austattungsName)
+           ON DELETE CASCADE,
 CONSTRAINT fk_Besitzt FOREIGN KEY (ferienwohnungsName)
-           REFERENCES Ferienwohnung(name)           
+           REFERENCES Ferienwohnung(name)
+           ON DELETE CASCADE
 );
 
+CREATE TABLE BILD
+(
+bildID varchar2(30),
+bildinhalt BLOB,
+ferienwohnungName varchar2(30),
 
-
-
-
-
+CONSTRAINT pk_bild PRIMARY KEY (bildID),
+CONSTRAINT fk_bild FOREIGN KEY (ferienwohnungName)
+           REFERENCES Ferienwohnung(name)
+           ON DELETE CASCADE
+);
 
