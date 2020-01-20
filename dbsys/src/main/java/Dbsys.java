4a60be1918fc9bc2865ebee47abec385df1bf6d6
Animesh Sharma <anim_esh@yahoo.co.in>
2020-01-20 21:20:41 +0100

    ++ dbsys complete for this sem ++

--- dbsys/src/main/java/Dbsys.java
+++ dbsys/src/main/java/Dbsys.java
@@ -9,10 +9,10 @@ public class Dbsys {
     static String name = null;
     static String passwd = null;
 
-    static String land = "Spanien";
-    static String startDatum = "01.11.2016";
-    static String endDatum = "21.11.2016";
-    static String ausstattung = "Sauna";
+    static String land = "";
+    static String startDatum = "";
+    static String endDatum = "";
+    static String ausstattung = "";
     static String email = "";
 
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -67,110 +67,150 @@ public class Dbsys {
             conn.setAutoCommit(false);                                                    // Kein automatisches Commit
             stmt = conn.createStatement();
 
-           rset = stmt.executeQuery(queryForFerienWohnung(startDatum, endDatum, ausstattung, land));
-//         rset = stmt.executeQuery(testQuery());
-           System.out.println(rset.toString());
+            System.out.println("Press s: to search, b: to book or e: to exit");
+            while (true) {
+                switch (in.readLine()) {
+
+                    case "s": {
+                        stmt = conn.createStatement();
+
+                        System.out.println("Land-\n");
+                        land = in.readLine();
+
+                        System.out.println(" Please enter start date in format dd.mm.yyyy - \n");
+                        startDatum = in.readLine();
+
+                        System.out.println(" Please enter end date in format dd.mm.yyyy - \n");
+                        endDatum = in.readLine();
+
+                        System.out.println("Facilities -");
+                        ausstattung = in.readLine();
+
+                        rset = stmt.executeQuery(queryForFerienWohnung(startDatum, endDatum, ausstattung, land));
+
+                        while (rset.next()) {
+
+                            System.out.println(" Name - " + rset.getString("name"));
+                            System.out.println();
+                        }
+
+                        conn.commit();
+                        stmt.close();
+
+                    }
+                    break;
+                    case "b": {
+                        stmt = conn.createStatement();
+
+                        System.out.println("-----------------------\n");
+
+                        System.out.println("please enter email addresse-");
+                        email = in.readLine();
+
+                        if (istEineNeueKunde(email, stmt)) {
+
+                            System.out.println("We could not find you in our database please enter credentials - ");
 
 
+                            System.out.println("Newsletter-");
+                            newsletter = in.readLine();
 
-            while (rset.next()) {
+                            System.out.println("passwort-");
+                            passwort = in.readLine();
 
-                System.out.println(rset.getString("name") + "----" + rset.getString("AVG(be.sterne)"));
-                System.out.println();
-            }
+                            System.out.println("Name-");
+                            kundname = in.readLine();
 
+                            System.out.println("iban-");
+                            iban = in.readLine();
 
-            System.out.println("please enter email addresse-");
-            email = in.readLine();
+                            System.out.println("Strasse-");
+                            strasse = in.readLine();
 
-            if(istEineNeueKunde(email,stmt)){
+                            System.out.println("plz");
+                            plz = in.readLine();
 
-                System.out.println("We could not find you in our database please enter credentials - ");
+                            System.out.println("ort/city name-");
+                            ort = in.readLine();
 
+                            System.out.println("landname-");
+                            landname = in.readLine();
 
-                System.out.println("Newsletter-");
-                newsletter = in.readLine();
+                            System.out.println("hausnummer-");
+                            hausnummer = in.readLine();
 
-                System.out.println("passwort-");
-                passwort = in.readLine();
+                            strassenr = strasse + hausnummer + plz;
 
-                System.out.println("Name-");
-                kundname = in.readLine();
+                            adr_id = String.valueOf(Math.abs(strassenr.hashCode()));
 
-                System.out.println("iban-");
-                iban = in.readLine();
+                            registerNeueKunde(newsletter, passwort, kundname, iban, strasse, plz, ort, hausnummer, landname, stmt);
 
-                System.out.println("Strasse-");
-                strasse = in.readLine();
+                        }
 
-                System.out.println("plz");
-                plz = in.readLine();
+                        System.out.println("Welcome Back \n");
+                        showAlleFerienWohnungsName(stmt);
+                        System.out.println(" Please enter a Ferienwohnungsname - \n");
 
-                System.out.println("ort/city name-");
-                ort = in.readLine();
+                        ferienwohnungsName = in.readLine();
 
-                System.out.println("landname-");
-                landname = in.readLine();
-
-                System.out.println("hausnummer-");
-                hausnummer = in.readLine();
-
-                strassenr = strasse + hausnummer + plz;
-
-                adr_id = String.valueOf(Math.abs(strassenr.hashCode()));
-
-                registerNeueKunde(newsletter,passwort,kundname,iban,strasse,plz,ort,hausnummer,landname,stmt);
-
-            }
-
-            System.out.println("Welcome Back \n");
-            showAlleFerienWohnungsName(stmt);
-            System.out.println(" Please enter a Ferienwohnungsname - \n");
-
-            ferienwohnungsName = in.readLine();
-
-            if(hashMapWithNameUndPreis.containsKey(ferienwohnungsName)){
+                        if (hashMapWithNameUndPreis.containsKey(ferienwohnungsName)) {
 
 
-                System.out.println(" Please enter arrival date in  format dd.mm.yyyy - \n");
-                anDatum = in.readLine();
+                            System.out.println(" Please enter arrival date in  format dd.mm.yyyy - \n");
+                            anDatum = in.readLine();
 
-                System.out.println(" Please enter departure date in  format dd.mm.yyyy - \n");
-                abDatum = in.readLine();
+                            System.out.println(" Please enter departure date in  format dd.mm.yyyy - \n");
+                            abDatum = in.readLine();
 
-                long dates = DateUtils.compareDates(anDatum,abDatum);
-                float preisProTag = hashMapWithNameUndPreis.get(ferienwohnungsName);
-                float summe = (dates * preisProTag);
+                            long dates = DateUtils.compareDates(anDatum, abDatum);
+                            float preisProTag = hashMapWithNameUndPreis.get(ferienwohnungsName);
+                            float summe = (dates * preisProTag);
 
-                preis = String.valueOf((int)summe);
+                            preis = String.valueOf((int) summe);
 
-                System.out.println("total preis " + preis);
+                            System.out.println("total preis " + preis);
 
-                int time = Long.hashCode(System.currentTimeMillis());
+                            int time = Long.hashCode(System.currentTimeMillis());
 
-                if (time < 0) {
-                    time = time * -1;
-                    time = (time / 10000);
-                } else {
-                    time = time / 10000;
+                            if (time < 0) {
+                                time = time * -1;
+                                time = (time / 10000);
+                            } else {
+                                time = time / 10000;
+                            }
+
+                            buchung_id = String.valueOf(time);
+                            System.out.println(buchung_id);
+
+                            stmt.executeQuery(insertIntoBuchung(buchung_id, anDatum, abDatum, ferienwohnungsName, email, preis));
+
+                            System.out.println(" Booking successful - \n");
+
+                        } else {
+
+                            System.out.println(" Please enter a Ferienwohnungsname from the list - \n");
+
+                        }
+
+                        conn.commit();
+                        stmt.close();
+
+                    }
+                    break;
+
+                    case "e":  {
+                        conn.close();
+                        System.exit(-1);
+                        break;
+                    }
+
+                    default: {
+
+                        System.out.println("Please select a search, book or exit function");
+                    }
+
                 }
-
-                buchung_id = String.valueOf(time);
-                System.out.println(buchung_id);
-
-                stmt.executeQuery(insertIntoBuchung(buchung_id,anDatum,abDatum,ferienwohnungsName,email,preis));
-
-                System.out.println(" Booking successful - \n");
-
-            } else {
-
-                System.out.println(" Please enter a Ferienwohnungsname from the list - \n");
-
             }
-
-
-            conn.commit();
-            conn.close();
 
         } catch (SQLException se) {                                                        // SQL-Fehler abfangen
             System.out.println();
@@ -273,24 +313,13 @@ public class Dbsys {
         return "INSERT INTO Dbsys51.Buchung VALUES (" + time + ",to_date(sysdate, 'DD.MM.YYYY'),to_date('" + abDatum + "', 'DD.MM.YYYY'),to_date('" + anDatum + "', 'DD.MM.YYYY'),'" + ferienwohnungsName + "','" + email + "' ,'" + betrag + "', " + time + ", '', to_date(sysdate, 'DD.MM.YYYY'))";
     }
 
-    private static String testQuery() {
-
-        return "SELECT a.landname, NVL(COUNT(b.FERIENWOHNUNGSNAME), 0)\n" +
-                "FROM DBSYS51.Buchung b INNER JOIN DBSYS51.Ferienwohnung f ON b.FERIENWOHNUNGSNAME = f.name \n" +
-                "RIGHT OUTER JOIN DBSYS51.Adresse a ON a.addressid = f.addressid\n" +
-                "GROUP BY a.landname\n" +
-                "ORDER BY NVL(COUNT(b.buchungsnummer), 0) DESC";
-
-    }
-
     private static String queryForFerienWohnung(String startDatum, String endDatum, String austtatung, String land) {
 
-        if (!startDatum.isEmpty() && !endDatum.isEmpty() && !austtatung.isEmpty() && !land.isEmpty()) {
+        if (!startDatum.isEmpty() && !endDatum.isEmpty() && !land.isEmpty() && !austtatung.isEmpty()) {
 
-           return "SELECT f.name, AVG(be.sterne)\n" +
-                    "FROM DBSYS51.ferienwohnung f, DBSYS51.buchung b, DBSYS51.adresse a, DBSYS51.Bewertung be\n" +
+           return "SELECT f.name \n" +
+                    "FROM DBSYS51.ferienwohnung f left outer join DBSYS51.buchung b on f.name = b.ferienwohnungsname, DBSYS51.adresse a \n" +
                     "WHERE\n" +
-                    "    b.ferienwohnungsname = f.name AND\n" +
                     "    NOT EXISTS( \n" +
                     "                SELECT be.ferienwohnungsname\n" +
                     "                FROM DBSYS51.buchung be WHERE\n" +
@@ -299,9 +328,22 @@ public class Dbsys {
                     "    AND f.addressId = a.addressId \n" +
                     "    AND a.landname = '"+land+"'\n" +
                     "    AND b.ferienwohnungsname IN (SELECT bes.ferienwohnungsname FROM DBSYS51.besitzt bes WHERE bes.austattungsname = '"+austtatung+"')\n" +
-                    "    AND b.BEWERTUNGSID = be.BEWERTUNGSID\n" +
-                    "GROUP BY f.name\n" +
-                    "ORDER BY AVG(be.sterne) DESC";
+                    " \n" +
+                    "GROUP BY f.name";
+
+        } else if (!startDatum.isEmpty() && !endDatum.isEmpty() && !land.isEmpty()) {
+
+            return "SELECT f.name \n" +
+                    "FROM DBSYS51.ferienwohnung f left outer join DBSYS51.buchung b on f.name = b.ferienwohnungsname, DBSYS51.adresse a \n" +
+                    "WHERE\n" +
+                    "    NOT EXISTS( \n" +
+                    "                SELECT be.ferienwohnungsname\n" +
+                    "                FROM DBSYS51.buchung be WHERE\n" +
+                    "                (be.abreisedatum > to_date('"+endDatum+"', 'DD.MM.YYYY') AND be.anreisedatum < to_date('"+startDatum+"', 'DD.MM.YYYY'))\n" +
+                    "                OR(be.anreisedatum > to_date('"+endDatum+"', 'DD.MM.YYYY') AND be.abreisedatum < to_date('"+startDatum+"', 'DD.MM.YYYY')))\n" +
+                    "    AND f.addressId = a.addressId \n" +
+                    "    AND a.landname = '"+land+"'\n" +
+                    "    GROUP BY f.name";
         }
 
         return "LOL";
